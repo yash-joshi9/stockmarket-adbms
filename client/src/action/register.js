d5206ad1775f2a8fee32a5491d6f4a84d4e99abb
mi6111317 <milishah1213@gmail.com>
2021-03-07 19:10:29 -0600

    Register login action file

---
+++ client/src/action/register.js
@@ -0,0 +1,48 @@
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+const host = process.env.REACT_APP_REACT_HOST;
+
+
+export const register = (payload) => {
+  return {
+      type: "REGISTER_USER",
+      payload
+  }
+}
+
+
+export const registerUser = (values) => async (dispatch) => {
+   
+    let url = `${host}/users`
+    console.log(values, ">>>>>>>>>>>>>>")
+    let {name, email, password, phoneNumber } = values;
+    password = btoa(password);
+    const data = {name, email, password, phoneNumber}
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        origin: "*"
+      },
+      body: JSON.stringify(data),
+      redirect: 'follow'
+    })
+      .then(response => response.json())
+      .then(async (result) => {
+
+        if(result.error) {
+          return dispatch(register(result.error)) 
+        }
+        const { token, user } = result;
+        const { _id } = user;
+        cookies.set('authToken', token);
+        cookies.set('userId', _id);
+
+        window.location.href = "/dashboard";
+      })
+      .catch(error => console.log('error', error));
+}
+
+
