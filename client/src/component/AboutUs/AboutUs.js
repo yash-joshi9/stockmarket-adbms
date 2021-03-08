7714dbecad799c47232e1f7d3bf680d71c4b7ed7
mi6111317 <milishah1213@gmail.com>
2021-03-07 19:26:06 -0600

    About us page and design

---
+++ client/src/component/AboutUs/AboutUs.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./style.scss";
+
+function AboutUs(props) {
+  return (
+    <div className="about-us-section">
+      <div className="heading-wrapper">
+        ASE FINAL PROJECT
+      </div>
+      <div className="wrap-team-members">
+        <div className="team-members">
+            Ashmit Samra
+        </div>
+        <div className="team-members">
+            Prasiddha Patel
+        </div>
+        <div className="team-members">
+            Yash Joshi
+        </div>
+        <div className="team-members">
+          Nikhil Kothari
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default AboutUs;
