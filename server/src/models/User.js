1a9ad7b19de4203617b353744b4cad288da5115e
mi6111317 <milishah1213@gmail.com>
2021-03-07 19:14:37 -0600

    User Model

---
+++ server/src/models/User.js
@@ -0,0 +1,94 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require('dotenv').config()
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate(email) {
+      if (!validator.isEmail(email)) {
+        throw new Error("Email is not invalid");
+      }
+    },
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  isArchived: {
+    type: Boolean,
+    default: false,
+    trim: true,
+  },
+  tokens: [
+    {
+      token: {
+        required: true,
+        type: String,
+      },
+    },
+  ],
+  time: { type: Date, default: Date.now },
+});
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user.id.toString() }, process.env.AUTH_TOKEN_KEY);
+  user.tokens = user.tokens.concat({
+    token,
+  });
+  await user.save();
+  return token;
+};
+
+//model  method
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
