6e6dced73fe222f0820fe729fca5f0c0f3ab7987
Yash Joshi <yjoshi.2098@gmail.com>
2021-03-07 19:07:43 -0600

    Created modal for login signup

---
+++ client/src/component/login-signup-modal/loginSignUpModal.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import Modal from "../Modal/Modal";
+import LoginContainer from "../../container/LoginContainer";
+import SignUpContainer from '../../container/signUpContainer';
+import "./style.scss";
+
+
+export default function LoginSignUpModal(props) {
+
+    const [isLogin, setisLogin] = useState(false)
+
+    const { onHandleShowLoginSignUp, onIsShowLoginSignUp } = props;
+
+    const showLogin = () => {
+        setisLogin(true)
+    }
+
+    
+    const showSignUp = () => {
+        setisLogin(false)
+    }
+
+    return (
+        <Modal
+            open={onIsShowLoginSignUp}
+            ownClass="login-signup-modal"
+            onClose={() => onHandleShowLoginSignUp(false)}
+        >
+            <div className="login-signup-container">
+                {/* <div className="header-bar">
+                    <div
+                        className={`header-text ${isLogin ? "is-selected" : ""}`}
+                        onClick={() => setisLogin(true)}>
+                        Login
+                    </div>
+                    <div
+                        className={`header-text ${isLogin ? "" : "is-selected"}`}
+                        onClick={() => setisLogin(false)}>
+                        Sign-Up
+                    </div>
+                </div> */}
+
+                {isLogin
+                    ?
+                    <LoginContainer 
+                        showSignUp={showSignUp}
+                    />
+                    :
+                    <SignUpContainer 
+                        showLogin={showLogin}
+                    />
+                }
+            </div>
+        </Modal>
+    )
+}
