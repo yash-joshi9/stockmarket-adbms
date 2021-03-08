fa425b0ea8350d35dc39eb51b9bd0057f24207e9
Yash Joshi <yjoshi.2098@gmail.com>
2021-03-07 19:15:04 -0600

    Added middelware

---
+++ server/src/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+require('dotenv').config()
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.AUTH_TOKEN_KEY);
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+    if (!User) {
+      throw new Error();
+    }
+    req.user = user;
+    req.token = token;
+    next();
+  } catch {
+    res.status(401).send({
+      error: "please authenticate",
+    });
+  }
+};
+module.exports = auth;
