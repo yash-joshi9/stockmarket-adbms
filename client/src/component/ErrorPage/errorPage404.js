bf8e3db5b4cfd75097cd2c313f569dcefe40dbee
bhagath97 <hbvpus@gmail.com>
2021-03-07 19:26:45 -0600

    Error page and its design

---
+++ client/src/component/ErrorPage/errorPage404.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "./style.scss";
+
+
+function ErrorPage404(props) {
+
+    return (
+        <div className="Error-section">
+            <div className="text">
+                    404 page not found
+                </div>
+        </div>
+    );
+}
+export default ErrorPage404;
