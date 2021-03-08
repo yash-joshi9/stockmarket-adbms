7cba3299c6f6a15215e0763537b9e53913ebea0f
bhagath97 <hbvpus@gmail.com>
2021-03-07 19:23:40 -0600

    Index file and design

---
+++ client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { applyMiddleware, compose, createStore } from 'redux';
+import allReducers from './reducer';
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+
+
+export const store = createStore(allReducers, compose(applyMiddleware(thunk)))
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
