b37f4e5f46bb259d02366d5daad00aee429d2183
Ashmit Samra <Samraashmit@outlook.com>
2021-03-07 19:27:13 -0600

    Landing page component with css file

---
+++ client/src/component/LandingPage/landingPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.scss";
+
+function LandingPage(props) {
+  return (
+    <div className="landing-section">
+      <div className="intro-main">
+        <div className="title1">
+          Fantasy League welcomes you to an amazing virtual cricket league.
+          <br/>Connect with other players
+          <br/>&
+          <br/>Test your GAMING SKILLS.
+        </div>
+        <div className="title2">
+          Build your own team here,
+          <br/>Get a real life experience with multiple players collaborating as a team.
+        </div>
+      </div>
+    </div>
+  );
+}
+export default LandingPage;
